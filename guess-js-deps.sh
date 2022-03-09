5f1e1bf8e5e82b8a396915849f08dc796dce1ae2
mk-pmb <mk-pmb@users.noreply.github.com>
2022-03-09 05:17:20 +0100

    v0.1.55: Remove accidential debug logging

--- guess-js-deps.sh
+++ guess-js-deps.sh
@@ -158,7 +158,6 @@ function find_manif_eslint_deps () {
   local BUF="$DEPS"
   BUF="¶ ${BUF//$'\n'/ ¶ } ¶"
   BUF="${BUF//$'\t'/ » }"
-  local -p >&2
   case "$BUF" in
     *'¶ eslint-config-nodejs-pmb '* )
       <<<"$SCRIPTS" grep -qoPe '^\s*"elp[ \&"]' && DEPS+=$'\neslint-pretty-pmb'
